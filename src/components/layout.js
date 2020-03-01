ed5eb716e82dbd2e308df9e0cd5b8191d34e0717
Kevin Adam <rexprimeadam@gmail.com>
2020-03-01 23:02:40 +0700

    offline functionality

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,22 +7,11 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children, isback, action }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <React.Fragment>
       {isback && (
