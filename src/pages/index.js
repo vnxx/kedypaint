43370e858ebc18c423fcccbddbda748710e9a715
Kevin Adam <rexprimeadam@gmail.com>
2020-03-01 22:08:50 +0700

    update readme n index

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const IndexPage = () => (
           <Link to="/dashboard" className="py-3 shadow-main px-10 rounded-full text-1xl sm:text-2xl text-secondary inline-block font-bold bg-white">Let’s Get Started</Link>
         </div>
         <div className="flex mb-1 justify-center items-center text-sm">
-          <a className="flex" href="#" target="_blank" rel="noopener noreferrer"><Icons name='github' className='mr-2' /> github/vnxx/kedypaint</a>
+          <a className="flex hover:underline" href="https://github.com/vnxx/kedypaint" target="_blank" rel="noopener noreferrer"><Icons name='github' className='mr-2' /> github/vnxx/kedypaint</a>
         </div>
         <a href="https://bykevin.work"><p className="text-sm ">App by @keevnx</p></a>
       </div>
