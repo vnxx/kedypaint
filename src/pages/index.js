bef3304a1a162207f124431ef9f1a2653ba8aa82
Kevin Adam <rexprimeadam@gmail.com>
2020-05-27 17:12:35 +0700

    change author

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,10 +16,10 @@ const IndexPage = () => (
         <div className="flex mb-5 justify-center items-center">
           <Link to="/dashboard" className="py-3 shadow-main px-10 rounded-full text-1xl sm:text-2xl text-secondary inline-block font-bold bg-white">Let’s Get Started</Link>
         </div>
-        <div className="flex mb-1 justify-center items-center text-sm">
+        <div className="flex mb-3 justify-center items-center text-sm">
           <a className="flex hover:underline" href="https://github.com/vnxx/kedypaint" target="_blank" rel="noopener noreferrer"><Icons name='github' className='mr-2' /> github/vnxx/kedypaint</a>
         </div>
-        <a href="https://bykevin.work"><p className="text-sm ">App by @keevnx</p></a>
+        <p className="text-sm">Komputer Masyarakat | Kelompok 3</p>
       </div>
     </div>
   </React.Fragment>
